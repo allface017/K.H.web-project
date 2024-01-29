3fe1a52192fed12962723697196f49e3db713aa0
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-01-29 11:23:09 +0900

    workflow.htmlのhtml作成

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def next():
 def sample():
     return render_template('index.html')
 
+@app.route('/work')
+def work():
+    return render_template('Workflow.html')
 
 # if __name__ == "__main__":
     # app.run(debug=True) # サーバー起動
